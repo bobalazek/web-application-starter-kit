59673b5c1362231a76112ab70e3f0e8508b4a3df
Borut Balazek <bobalazek124@gmail.com>
2015-05-01 11:09:54 +0200

    Version bump

--- app/configs/global.php
+++ app/configs/global.php
@@ -4,7 +4,7 @@ return array(
     'environment' => 'development',
     'debug' => true,
     'name' => 'Web Application Starter Kit',
-    'version' => '0.1.2',
+    'version' => '0.1.3',
     'author' => 'Corcosoft',
 
     // Admin email (& name)
