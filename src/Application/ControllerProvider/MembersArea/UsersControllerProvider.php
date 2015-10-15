74591b534c6093b8a851c7ef20623846095cbb12
Borut Balažek <bobalazek124@gmail.com>
2015-10-15 16:07:25 +0200

    Update UsersControllerProvider.php

--- src/Application/ControllerProvider/MembersArea/UsersControllerProvider.php
+++ src/Application/ControllerProvider/MembersArea/UsersControllerProvider.php
@@ -24,68 +24,6 @@ class UsersControllerProvider implements ControllerProviderInterface
         ->bind('members-area.users.new');
 
         $controllers->match(
-            '/actions',
-            'Application\Controller\MembersArea\UsersController::actionsAction'
-        )
-        ->bind('members-area.users.actions');
-
-        /***** Notifications *****/
-        $controllers->match(
-            '/notifications',
-            'Application\Controller\MembersArea\Users\NotificationsController::indexAction'
-        )
-        ->bind('members-area.users.notifications');
-
-        $controllers->match(
-            '/notifications/new',
-            'Application\Controller\MembersArea\Users\NotificationsController::newAction'
-        )
-        ->bind('members-area.users.notifications.new');
-
-        $controllers->match(
-            '/notifications/{id}/edit',
-            'Application\Controller\MembersArea\Users\NotificationsController::editAction'
-        )
-        ->bind('members-area.users.notifications.edit');
-
-        $controllers->match(
-            '/notifications/{id}/remove',
-            'Application\Controller\MembersArea\Users\NotificationsController::removeAction'
-        )
-        ->bind('members-area.users.notifications.remove');
-
-        /***** Messages *****/
-        $controllers->match(
-            '/messages',
-            'Application\Controller\MembersArea\Users\MessagesController::indexAction'
-        )
-        ->bind('members-area.users.messages');
-
-        $controllers->match(
-            '/messages/new',
-            'Application\Controller\MembersArea\Users\MessagesController::newAction'
-        )
-        ->bind('members-area.users.messages.new');
-
-        $controllers->match(
-            '/messages/{id}/edit',
-            'Application\Controller\MembersArea\Users\MessagesController::editAction'
-        )
-        ->bind('members-area.users.messages.edit');
-
-        $controllers->match(
-            '/messages/{id}/remove',
-            'Application\Controller\MembersArea\Users\MessagesController::removeAction'
-        )
-        ->bind('members-area.users.messages.remove');
-
-        $controllers->match(
-            '/oauth-services',
-            'Application\Controller\MembersArea\UsersController::oauthServicesAction'
-        )
-        ->bind('members-area.users.oauth-services');
-
-        $controllers->match(
             '/{id}',
             'Application\Controller\MembersArea\UsersController::detailAction'
         )
@@ -102,18 +40,6 @@ class UsersControllerProvider implements ControllerProviderInterface
             'Application\Controller\MembersArea\UsersController::removeAction'
         )
         ->bind('members-area.users.remove');
-
-        $controllers->match(
-            '/{id}/lock',
-            'Application\Controller\MembersArea\UsersController::lockAction'
-        )
-        ->bind('members-area.users.lock');
-
-        $controllers->match(
-            '/{id}/unlock',
-            'Application\Controller\MembersArea\UsersController::unlockAction'
-        )
-        ->bind('members-area.users.unlock');
 
         return $controllers;
     }
