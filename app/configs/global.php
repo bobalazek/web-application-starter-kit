cdf045963f374a5a26d16dcf8dbd61fe46c7f1ae
Borut Balazek <bobalazek124@gmail.com>
2015-12-25 16:21:37 +0100

    Version bump

--- app/configs/global.php
+++ app/configs/global.php
@@ -4,7 +4,7 @@ return array(
     'environment' => 'development',
     'debug' => true,
     'name' => 'Web Application Starter Kit',
-    'version' => '0.7.1',
+    'version' => '0.7.2',
     'author' => 'Borut Balazek',
 
     // Admin email (& name)
