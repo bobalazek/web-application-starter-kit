7f3588a6101bdb27458814ea24d38f403fef1ea3
Borut Balažek <bobalazek124@gmail.com>
2016-01-09 19:36:28 +0100

    Update UserRepository.php

--- src/Application/Repository/UserRepository.php
+++ src/Application/Repository/UserRepository.php
@@ -9,6 +9,9 @@ use Doctrine\ORM\EntityRepository;
  */
 class UserRepository extends EntityRepository
 {
+    /**
+     * @return integer
+     */
     public function countAll()
     {
         return $this->createQueryBuilder('u')
