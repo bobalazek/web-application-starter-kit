aa9bed10fdf35160f8e6b8a253ebb1516ae2bbc6
Borut Balažek <bobalazek124@gmail.com>
2016-01-09 19:36:54 +0100

    Update PostMetaRepository.php

--- src/Application/Repository/PostMetaRepository.php
+++ src/Application/Repository/PostMetaRepository.php
@@ -9,6 +9,9 @@ use Doctrine\ORM\EntityRepository;
  */
 class PostMetaRepository extends EntityRepository
 {
+    /**
+     * @return integer
+     */
     public function countAll()
     {
         return $this->createQueryBuilder('pm')
