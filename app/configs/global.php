dc64267570a38b8255b1353bfa1e7531d74747d1
Borut Balazek <bobalazek124@gmail.com>
2016-03-07 20:07:40 +0100

    Added php cs fixer dotfile

--- app/configs/global.php
+++ app/configs/global.php
@@ -4,7 +4,7 @@ return array(
     'environment' => 'development',
     'debug' => true,
     'name' => 'Web Application Starter Kit',
-    'version' => '1.0.0-rc.2',
+    'version' => '1.0.0-rc.3',
     'author' => 'Borut Balazek',
 
     // Admin email (& name)
