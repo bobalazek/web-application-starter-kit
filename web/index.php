cbd5c2c9ef05ff7c5b28eae636866d084b39da18
Borut Balazek <bobalazek124@gmail.com>
2017-08-21 15:58:23 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Symfony\Component\Debug\Debug;
+
 // Used for php web server
 $filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
 if (php_sapi_name() === 'cli-server' && is_file($filename)) {
@@ -9,6 +11,8 @@ if (php_sapi_name() === 'cli-server' && is_file($filename)) {
 $app = require dirname(dirname(__FILE__)).'/app/bootstrap.php';
 
 if ($app['debug']) {
+    Debug::enable();
+
     $app->run();
 } else {
     $app['http_cache']->run();
